0b4792d904e4e65708097dd35a41b81cfa12d39a
ViniciusPereira11 <117477222+ViniciusPereira11@users.noreply.github.com>
2023-06-30 13:48:33 -0400

    Add files via upload

---
+++ DeteccaoDeObjetos/human.js
@@ -0,0 +1,8 @@
+function preload(){
+
+}
+function setup(){
+    canvas = createCanvas(640, 480);
+    canvas.position(windowWidth/2 - 320 ,200);
+    background("white");
+}
---
+++ DeteccaoDeObjetos/lm.js
@@ -0,0 +1,5 @@
+function setup(){
+    canvas = createCanvas(640, 480);
+    canvas.position(windowWidth/2 - 320 ,200);
+    background("white");
+}
